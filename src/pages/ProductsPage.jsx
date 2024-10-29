3cf95be06ed4321625edc963a01ef45dba5802df
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-10-29 21:25:27 +0330

    create custom hook to get data from the context

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,9 @@
+import { useProducts } from "../context/PruductContext";
+
 function ProductsPage() {
+  const products = useProducts();
+  console.log(products);
+  
   return <div>ProductsPage</div>;
 }
 
