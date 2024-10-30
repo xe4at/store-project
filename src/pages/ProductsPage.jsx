4eeab1fce3f19f29e34284799b33009299f7a9bc
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-10-30 17:51:05 +0330

    creat Card component and add styles to it

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,17 +1,18 @@
+import Card from "../components/Card";
 import { useProducts } from "../context/PruductContext";
 
 import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
   const products = useProducts();
-  console.log(products);
+  // console.log(products);
 
   return (
     <div className={styles.container}>
       <div className={styles.products}>
         {!products.length && <p>Loading...</p>}
         {products.map((p) => (
-          <p key={p.id}>{p.title}</p>
+          <Card key={p.id} data={p} />
         ))}
       </div>
       <div>sidebar</div>
