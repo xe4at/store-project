272d2f5ba65f326c76a96fd88c701b8cbdbef6f6
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-10-30 18:07:13 +0330

    add loader to project

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,5 @@
 import Card from "../components/Card";
+import Loader from "../components/Loader";
 import { useProducts } from "../context/PruductContext";
 
 import styles from "./ProductsPage.module.css";
@@ -10,7 +11,7 @@ function ProductsPage() {
   return (
     <div className={styles.container}>
       <div className={styles.products}>
-        {!products.length && <p>Loading...</p>}
+        {!products.length && <Loader />}
         {products.map((p) => (
           <Card key={p.id} data={p} />
         ))}
