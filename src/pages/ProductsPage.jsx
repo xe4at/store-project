5227b2dd05c362e0cf7f99db586ee680989e20ec
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-10-30 18:20:41 +0330

    add searchbox to project

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,23 +1,43 @@
+import { ImSearch } from "react-icons/im";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/PruductContext";
 
 import styles from "./ProductsPage.module.css";
+import { useState } from "react";
 
 function ProductsPage() {
   const products = useProducts();
-  // console.log(products);
+
+  const [search, setSearch] = useState("");
+
+  const searchHandler = () => {
+    console.log("search");
+  };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div>sidebar</div>
+      </div>
+    </>
   );
 }
 
