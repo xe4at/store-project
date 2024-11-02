394e596bc36c301e873a92374ef9426f82941c66
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-11-02 20:17:49 +0330

    create query state

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -12,13 +12,18 @@ function ProductsPage() {
 
   const [displayed, setDisplayed] = useState([]);
   const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
 
   useEffect(() => {
     setDisplayed(products);
   }, [products]);
 
+  useEffect(() => {
+    console.log(query);
+  }, [query]);
+
   const searchHandler = () => {
-    console.log("search");
+    setQuery((query) => ({ ...query, search }));
   };
 
   const categoryHandler = (event) => {
@@ -26,7 +31,7 @@ function ProductsPage() {
     const category = event.target.innerText.toLowerCase();
 
     if (tagName !== "LI") return;
-    console.log(category);
+    setQuery((query) => ({ ...query, category }));
   };
 
   return (
