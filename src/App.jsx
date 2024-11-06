23d589e3d61c196c348c202796806f5527c03c43
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-11-06 15:33:34 +0330

    create layout file and add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+
+import Layout from "./layout/Layout";
 import ProductsPage from "./pages/ProductsPage";
 import DetailsPage from "./pages/DetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
@@ -10,13 +12,15 @@ function App() {
   return (
     <CardProvider>
       <ProductsProvider>
-        <Routes>
-          <Route index element={<Navigate to="/products" replace />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/products/:id" element={<DetailsPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/*" element={<PageNotFound />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route index element={<Navigate to="/products" replace />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products/:id" element={<DetailsPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+        </Layout>
       </ProductsProvider>
     </CardProvider>
   );
