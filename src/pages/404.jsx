7e81110332b68e1a852595740cb8a1213a71341a
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-01-25 20:39:29 +0330

    change style

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,14 @@
+import React from 'react';
+import './PageNotFound.css';
+
 function PageNotFound() {
-  return <div>Page Not Found</div>;
+  return (
+    <div className="page-not-found">
+      <h1>404</h1>
+      <p>Page Not Found</p>
+      <a href="/" className="back-home">Go Back Home</a>
+    </div>
+  );
 }
 
 export default PageNotFound;
